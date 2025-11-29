279a4eb2614486ff2465dd4e97ed4e6c3273b957
APC9000 <willy.iglesia.s@gmail.com>
2025-11-29 02:20:17 -0800

    svelte config update

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 // svelte.config.js
 import adapter from '@sveltejs/adapter-vercel';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 export default {
   kit: {
